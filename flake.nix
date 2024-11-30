cccb88378b14b41b2785019f415dd446520ef3d4
dawson-b23 <dawsonhburgess@gmail.com>
2024-11-30 12:36:50 -0800

    git init........ last one for nixos i swear

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A Flake for my NixOS machine/server config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    prism = {
+      url = "github:IogaMaster/prism";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    recyclarr-configs = {
+      url = "github:recyclarr/config-templates";
+      flake = false;
+    };
+    secrets = {
+    };
+    nix-colors.url = "github:misterio77/nix-colors";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
